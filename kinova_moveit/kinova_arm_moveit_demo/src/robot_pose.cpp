b9df6f1bf64cfb6e446833a2a75857ceb2c372d4
Caio Amaral <caioaamaral@gmail.com>
2019-01-29 16:09:24 -0300

    now using sleep(5.0) to avoid execution aborts

--- kinova_moveit/kinova_arm_moveit_demo/src/robot_pose.cpp
+++ kinova_moveit/kinova_arm_moveit_demo/src/robot_pose.cpp
@@ -15,55 +15,59 @@ int main(int argc, char** argv){
     ros::NodeHandle nh;
 
     //get parameter from launch file
-    double x, y, z, w;
-    nh.getParam("x", x);
-    nh.getParam("y", y);
-    nh.getParam("z", z);
-    nh.getParam("w", w);
+    double X = 0, Y = 0, Z = 0, W = 0;
+    nh.getParam("x", X);
+    nh.getParam("y", Y);
+    nh.getParam("z", Z);
+    nh.getParam("w", W);
 
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
-    //defines planning group
+    //define planning group
     static const std::string PLANNING_GROUP = "arm";
     moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
 
-    //retrives robot_state info
+    //retrive robot_state info
     const robot_state::JointModelGroup* joint_model_group;
     joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
-    //gets actual end_effector position
+    //get actual end_effector position
     geometry_msgs::PoseStamped now_frame = move_group.getCurrentPose();
-
-    //pose Goal
-    planning_interface::MotionPlanRequest req;
-    planning_interface::MotionPlanResponse res;
     
+    //set goal position coordinates
     geometry_msgs::PoseStamped goal;
     goal.header.frame_id = "root";
-    goal.pose.position.x = x;
-    goal.pose.position.y = y;
-    goal.pose.position.z = z;
-    goal.pose.orientation.w = w;
+    goal.pose.position.x = X;
+    goal.pose.position.y = Y;
+    goal.pose.position.z = Z;
+    goal.pose.orientation.w = W;
 
     move_group.setPoseTarget(goal); 
 
+    //specify tolerances [x, y, z] -> [0.01, 0.01, 0.01]
     std::vector<double> tolerance_pose(3, 0.01);
     std::vector<double> tolerance_angle(3, 0.01);
 
+    move_group.setGoalPositionTolerance(0.001);
+    move_group.setGoalOrientationTolerance(0.001);
+
+    //compute the plan path
     moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
     bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-
     ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "SUCESS" : "FAILED");
 
+    sleep(5.0);
+
+    //execute the trajectory
     move_group.move();
 
-  now_frame = move_group.getCurrentPose();
-    x = now_frame.pose.position.x;
-    y = now_frame.pose.position.y;
-    z = now_frame.pose.position.z;
-    w = now_frame.pose.orientation.w; 
+    //get new position coordinates
+    now_frame = move_group.getCurrentPose();
+    double x = now_frame.pose.position.x;
+    double y = now_frame.pose.position.y;
+    double z = now_frame.pose.position.z;
+    double w = now_frame.pose.orientation.w; 
 
     ROS_INFO("Actual end-effector position is: \n x = %f, y = %f, z= %f, w= %f", x, y, z, w);  
 
